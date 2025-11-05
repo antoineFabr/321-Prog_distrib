00fe9b0a31d0ba82c7f9e48988d7627c25f69cc5
Antoine <163823977+antoineFabr@users.noreply.github.com>
2025-11-05 12:16:19 +0100

    Exercice 2

---
+++ perso/serialize/serialize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+
+Character gérard = new Character() { FirstName = "Gérard", LastName = "Depardieux", Description = "Un très gros monsieur"};
+
+var dataj = JsonSerializer.Serialize(gérard);
+
+System.IO.File.WriteAllText("gérard.json", dataj);
+
+public class Character
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Description { get; set; }
+    public Actor PlayedBy { get; set; }
+}
+public class Actor
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Country { get; set; }
+    public bool IsAlive { get; set; }
+}
